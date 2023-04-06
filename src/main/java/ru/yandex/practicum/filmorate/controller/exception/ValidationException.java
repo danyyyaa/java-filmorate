f24e47ecc6374968d05602511470133e4949d53f
danya <danissim00o@yandex.ru>
2023-04-06 14:41:34 +0700

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/exception/ValidationException.java
@@ -1,9 +1,6 @@
 package ru.yandex.practicum.filmorate.controller.exception;
 
 public class ValidationException extends RuntimeException {
-    public ValidationException (String s) {
-        super(s);
-    }
 
     public ValidationException () {
         super();
