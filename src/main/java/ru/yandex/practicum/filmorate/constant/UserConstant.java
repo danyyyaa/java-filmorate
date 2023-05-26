5dacf66e6ef9fa27efdf8ca5ebd044baed6c9976
Данил <118910569+danyyyaa@users.noreply.github.com>
2023-05-26 13:16:28 +0700

    Update UserConstant.java

--- src/main/java/ru/yandex/practicum/filmorate/constant/UserConstant.java
+++ src/main/java/ru/yandex/practicum/filmorate/constant/UserConstant.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.constant;
 public class UserConstant {
 
     public static final String USER_TABLE = "user_t";
-
     public static final String ID = "id";
     public static final String EMAIL = "email";
     public static final String LOGIN = "login";
