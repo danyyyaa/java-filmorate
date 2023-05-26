9e4df7e25019500cd3295c261506d4e7d6e22499
Данил <118910569+danyyyaa@users.noreply.github.com>
2023-05-26 13:15:58 +0700

    Update GenreConstant.java

--- src/main/java/ru/yandex/practicum/filmorate/constant/GenreConstant.java
+++ src/main/java/ru/yandex/practicum/filmorate/constant/GenreConstant.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.constant;
 public class GenreConstant {
 
     public static final String GENRE_TABLE = "genre_t";
-
     public static final String ID = "id";
     public static final String NAME = "name";
 }
